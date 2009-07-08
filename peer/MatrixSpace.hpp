17d06a91fd1b3aca49b3bffaf53aba9784638e7a
moriyoshi <>
2009-07-08 07:27:10 +0000

    * Remove redundant code (this is avaiable in peer/py_hash_support.hpp)

--- peer/MatrixSpace.hpp
+++ peer/MatrixSpace.hpp
@@ -27,36 +27,6 @@
 
 #include "filters.hpp"
 
-#include "get_mapper_mf.hpp"
-
-template<typename Tval_>
-struct get_mapper_mf<boost::python::object, Tval_>
-{
-#if HAVE_UNORDERED_MAP || HAVE_TR1_UNORDERED_MAP || HAVE_BOOST_UNORDERED_MAP_HPP
-    struct hasher: public std::unary_function<boost::python::object, std::size_t>
-    {
-        typedef boost::python::object argument_type;
-        typedef std::size_t result_type;
-
-        result_type operator()(const argument_type& arg) const
-        {
-            return static_cast<result_type>((long)PyObject_Hash(arg.ptr()));
-        }
-    };
-#endif
-
-#if HAVE_UNORDERED_MAP
-    typedef std::unordered_map<boost::python::object, Tval_, hasher> type;
-#elif HAVE_TR1_UNORDERED_MAP
-    typedef std::tr1::unordered_map<boost::python::object, Tval_, hasher> type;
-#elif HAVE_BOOST_UNORDERED_MAP_HPP
-    typedef boost::unordered_map<boost::python::object, Tval_, hasher> type;
-#else 
-    typedef std::map<boost::python::object, Tval_> type;
-#endif
-};
-
-
 namespace peer {
 
 struct MatrixSpaceBase
